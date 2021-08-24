1ca59f1e1d762e09083d2662feae5b67ccf8ad1d
Bruno <bruno.3av@gmail.com>
2021-08-24 09:43:07 -0300

    added Stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,54 +13,73 @@ class _LoginPageState extends State<LoginPage> {
   String password = '';
 
   onPressed() {
-    if (email == "b" && password == "1") {
+    if (email == "bruno" && password == "123") {
       Navigator.of(context).pushNamed("/home");
     } else {
       print("Login Incorreto");
     }
   }
 
+  background() {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Image.network(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                child: Image.asset("assets/logo.png"),
+              ),
+              SizedBox(height: 50),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Email'),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (text) {
+                  password = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Password'),
+              ),
+              ElevatedButton(onPressed: onPressed, child: Text("Entrar"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset("assets/logo.png"),
-                  ),
-                  SizedBox(height: 50),
-                  TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Email'),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Password'),
-                  ),
-                  ElevatedButton(onPressed: onPressed, child: Text("Entrar"))
-                ],
-              ),
-            ),
-          ),
+        body: Stack(
+          children: [
+            background(),
+            body(),
+          ],
         ),
       ),
     );
